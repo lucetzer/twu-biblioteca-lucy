09126546ecc139c68bdb871c37fd250d645464c5
Lucy Fang <lucyfang2@gmail.com>
2016-02-23 09:36:30 +0000

    Added test for printBookList

--- src/com/twu/biblioteca/WelcomeUser.java
+++ src/com/twu/biblioteca/WelcomeUser.java
@@ -3,10 +3,15 @@ package com.twu.biblioteca;
 public class WelcomeUser {
 
     String message = "Hello and welcome to Biblioteca! There is no friend as loyal as a book so find yours!";
+    String bookList;
 
     public String printMessage() {
         System.out.println(message);
         return message;
+    }
+
+    public String printBookList() {
+        return bookList;
     }
 
 }
--- test/com/twu/biblioteca/WelcomeUserTest.java
+++ test/com/twu/biblioteca/WelcomeUserTest.java
@@ -6,6 +6,13 @@ import static org.junit.Assert.assertEquals;
 public class WelcomeUserTest {
 
     String message = "Hello and welcome to Biblioteca! There is no friend as loyal as a book so find yours!";
+    String bookList = "Here is our booklist:\n\n" + "TITLE                      AUTHOR           YEAR\n" +
+        "The Catcher in the Rye     J.D. Salinger    1951\n" +
+        "To Kill a Mockingbird      Harper Lee       1960\n" +
+        "Life of Pi                 Yann Martel      2001\n" +
+        "The Prophet                Kahlil Gibran    1923\n" +
+        "The Social Animal          David Brooks     2011";
+
     WelcomeUser newUser = new WelcomeUser();
 
     @Test
@@ -13,4 +20,10 @@ public class WelcomeUserTest {
         assertEquals(message, newUser.printMessage());
     }
 
+
+    @Test
+    public void testprintBookList() {
+        System.out.println(bookList);
+        assertEquals(bookList, newUser.printBookList());
+    }
 }
