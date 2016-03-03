72b4f0f876763f47ebc6bcbd17e466ba15316a4d
Lucy Fang <lucyfang@eulucyfang.local>
2016-03-03 15:41:12 +0000

    can add books and movies to library

---
+++ test/com/twu/biblioteca/printList.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by lucyfang on 03/03/2016.
+ */
+public class printList {
+}
--- test/com/twu/biblioteca/testLibrary.java
+++ test/com/twu/biblioteca/testLibrary.java
@@ -1,15 +1,33 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+import static org.junit.Assert.assertEquals;
 
 public class testLibrary {
 
+    private Book book1 = Mockito.mock(Book.class);
+    private Book book2 = Mockito.mock(Book.class);
+    private Movie movie1 = Mockito.mock(Movie.class);
+    private Movie movie2 = Mockito.mock(Movie.class);
     Library library = new Library();
 
     @Test
-    public void canDisplayLibraryBooks
+    public void canAddBookToLibraryBookList() {
+        library.bookList.add(book1);
+        library.bookList.add(book2);
+        assertEquals(book1, library.bookList.get(0));
+        assertEquals(book2, library.bookList.get(1));
+        assertEquals(2, library.bookList.size());
+    }
 
-
-
+    @Test
+    public void canAddMovieToLibraryMovieList() {
+        library.movieList.add(movie1);
+        library.movieList.add(movie2);
+        assertEquals(movie1, library.movieList.get(0));
+        assertEquals(movie2, library.movieList.get(1));
+        assertEquals(2, library.movieList.size());
+    }
 
 }
